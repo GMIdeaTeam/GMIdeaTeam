99529aa03eb98bdbc52de030d3e5362d17ddd87d
zzadu <zzadu08@naver.com>
2022-11-16 14:03:21 +0900

    #6 모드 변환 시 시야 변경 및 필터 추가

--- Assets/Scripts/ModeController/ModeController.cs
+++ Assets/Scripts/ModeController/ModeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,53 @@ public class ModeController : MonoBehaviour
 {
     bool isEditMode = false;
 
+    public Camera readCamera;
+    public Camera editCamera;
+
     public bool checkEditMode()
     {
         return isEditMode;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // 다른 키로 바뀔 수도 있음
+        {
+            if (isEditMode)
+                EditToRead();
+            else
+                ReadToEdit();
+            
+            isEditMode = !isEditMode;
+        }
+    }
+
+    void ReadToEdit()
+    {
+        // 읽기 모드 -> 편집 모드
+        ChangeToEditCamera();
+    }
+
+    void EditToRead()
+    {
+        // 편집 모드 -> 읽기 모드
+        ChangeToReadCamera();
+    }
+
+    void ChangeToEditCamera()
+    {
+        readCamera.gameObject.SetActive(false);
+        editCamera.gameObject.SetActive(true);
+    }
+
+    void ChangeToReadCamera()
+    {
+        readCamera.gameObject.SetActive(true);
+        editCamera.gameObject.SetActive(false);
+    }
+
+    void ChangeSpeed()
+    {
+        // 이동 속도 변경
     }
 }
