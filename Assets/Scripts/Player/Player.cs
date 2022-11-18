8db1696e444826f44763d27d14e25394f736fe82
taihun9120 <taihun9120@gmail.com>
2022-11-18 22:44:27 +0900

    #10 캐릭터 애니메이션 적용 및 아트 리소스 임포트
    .gitignore도 수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,25 +5,51 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [field: SerializeField]
-    float MoveSpeed { get; set; } = 5f;
+    float MoveSpeed { get; set; } = 5f; // ModeController의 speed를 참조할 수 있도록 추후 변경
+
     Vector2 moveVector;
+
+    Animator playerAnimator;
+
+    public ModeController modeController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerMove();
+        PlayerMove();
     }
 
-    private void playerMove()
+    private void PlayerMove()
     {
         moveVector.x = Input.GetAxisRaw("Horizontal");
         moveVector.y = Input.GetAxisRaw("Vertical");
+
+        if (moveVector.x != 0)
+        {
+            playerAnimator.SetBool("isMoveX", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isMoveX", false);
+        }
+
+        if (moveVector.y != 0)
+        {
+            playerAnimator.SetBool("isMoveY", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isMoveY", false);
+        }
+
+        playerAnimator.SetFloat("inputX", moveVector.x);
+        playerAnimator.SetFloat("inputY", moveVector.y);
 
         transform.Translate(moveVector * Time.deltaTime * MoveSpeed);
     }
