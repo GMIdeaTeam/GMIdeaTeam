34cb7903595dcd9ad0f78c78c54a9d42801d80cd
taihun9120 <taihun9120@gmail.com>
2022-11-21 00:20:07 +0900

    이동 애니메이션 완성

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,55 +2,99 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Idea.Player
 {
-    [field: SerializeField]
-    float MoveSpeed { get; set; } = 5f; // ModeController의 speed를 참조할 수 있도록 추후 변경
-
-    Vector2 moveVector;
-
-    Animator playerAnimator;
-
-    public ModeController modeController;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Player : MonoBehaviour
     {
-        playerAnimator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMove();
-    }
-
-    private void PlayerMove()
-    {
-        moveVector.x = Input.GetAxisRaw("Horizontal");
-        moveVector.y = Input.GetAxisRaw("Vertical");
-
-        if (moveVector.x != 0)
+        public enum Direction
         {
-            playerAnimator.SetBool("isMoveX", true);
-        }
-        else
+            UP, RIGHT, DOWN, LEFT
+        };
+        [field: SerializeField]
+        float MoveSpeed { get; set; } = 5f; // ModeController의 speed를 참조할 수 있도록 추후 변경
+
+        Vector2 moveVector;
+        Direction direction = Direction.DOWN;
+
+        Animator playerAnimator;
+
+        public ModeController modeController;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            playerAnimator.SetBool("isMoveX", false);
+            playerAnimator = GetComponent<Animator>();
         }
 
-        if (moveVector.y != 0)
+        // Update is called once per frame
+        void Update()
         {
-            playerAnimator.SetBool("isMoveY", true);
-        }
-        else
-        {
-            playerAnimator.SetBool("isMoveY", false);
+            PlayerMove();
+            UpdateDirection();
+            UpdateAnimation();
         }
 
-        playerAnimator.SetFloat("inputX", moveVector.x);
-        playerAnimator.SetFloat("inputY", moveVector.y);
+        private void PlayerMove()
+        {
+            moveVector.x = Input.GetAxisRaw("Horizontal");
+            moveVector.y = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(moveVector * Time.deltaTime * MoveSpeed);
+            transform.Translate(moveVector * Time.deltaTime * MoveSpeed);
+        }
+
+        private void UpdateDirection()
+        {
+            if (moveVector.x > 0 && moveVector.y == 0)
+            {
+                direction = Direction.RIGHT;
+            }
+            else if (moveVector.x < 0 && moveVector.y == 0)
+            {
+                direction = Direction.LEFT;
+            }
+            else if (moveVector.y > 0 && moveVector.x == 0)
+            {
+                direction = Direction.UP;
+            }
+            else if (moveVector.y < 0 && moveVector.x == 0)
+            {
+                direction = Direction.DOWN;
+            }
+        }
+
+        private void UpdateAnimation()
+        {
+            //if (moveVector.x != 0)
+            //{
+            //    playerAnimator.SetBool("isMoveX", true);
+            //}
+            //else
+            //{
+            //    playerAnimator.SetBool("isMoveX", false);
+            //}
+
+            //if (moveVector.y != 0)
+            //{
+            //    playerAnimator.SetBool("isMoveY", true);
+            //}
+            //else
+            //{
+            //    playerAnimator.SetBool("isMoveY", false);
+            //}
+
+            //playerAnimator.SetFloat("inputX", moveVector.x);
+            //playerAnimator.SetFloat("inputY", moveVector.y);
+
+            if (moveVector.x != 0 || moveVector.y != 0)
+            {
+                playerAnimator.SetBool("isMove", true);
+            }
+            else
+            {
+                playerAnimator.SetBool("isMove", false);
+            }
+
+            playerAnimator.SetFloat("direction", (float)direction);
+        }
     }
 }
