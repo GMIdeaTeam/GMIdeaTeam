a2aa93957bbde2a07a63aa1fd4183972c029ff1b
zzadu <zzadu08@naver.com>
2022-12-02 00:37:06 +0900

    #6 모드 변경 시 플레이어 이동 속도 변경

--- Assets/Scripts/ModeController/ModeController.cs
+++ Assets/Scripts/ModeController/ModeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Idea.Player;
 using UnityEngine;
 
 namespace Idea.ModeController
@@ -8,20 +9,16 @@ namespace Idea.ModeController
     public class ModeController : MonoBehaviour
     {
         bool isEditMode = false;
-    
+
         public Camera readCamera;
         public Camera editCamera;
-    
+
+        public PlayerData playerData;
+
         public bool IsEditMode
         {
-            get
-            {
-                return isEditMode;
-            }
-            set
-            {
-                isEditMode = value;
-            }
+            get { return isEditMode; }
+            set { isEditMode = value; }
         }
 
         private void Update()
@@ -32,39 +29,42 @@ namespace Idea.ModeController
                     EditToRead();
                 else
                     ReadToEdit();
-                
+
+                ChangeSpeed();
                 IsEditMode = !IsEditMode;
             }
         }
-    
+
         private void ReadToEdit()
         {
             // 읽기 모드 -> 편집 모드
             ChangeToEditCamera();
         }
-    
+
         private void EditToRead()
         {
             // 편집 모드 -> 읽기 모드
             ChangeToReadCamera();
         }
-    
+
         private void ChangeToEditCamera()
         {
             readCamera.gameObject.SetActive(false);
             editCamera.gameObject.SetActive(true);
         }
-    
+
         private void ChangeToReadCamera()
         {
             readCamera.gameObject.SetActive(true);
             editCamera.gameObject.SetActive(false);
         }
-    
+        
         private void ChangeSpeed()
         {
-            // 이동 속도 변경
+            if (!isEditMode) // 읽기 -> 편집
+                playerData.MoveSpeed = 3f; // 추후 변경
+            else // 편집 -> 읽기
+                playerData.MoveSpeed = 5f;
         }
     }
-
 }
